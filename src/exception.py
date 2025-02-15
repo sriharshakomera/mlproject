febdc1ce68245a00374853a017f9c6d0f0e34105
sriharshakomera <komerasriharsha@gmail.com>
2025-02-15 21:32:38 +0530

    Change to logging in exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -24,6 +24,6 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info("Logging has started")
+        logging.info("Divide by zero")
         raise CustomException(e,sys)
 '''
