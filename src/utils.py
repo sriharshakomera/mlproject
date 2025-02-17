817b44b3f85cb3e038ae30671909551bb16dbdb8
sriharshakomera <komerasriharsha@gmail.com>
2025-02-17 09:58:38 +0530

    Local Deployment

--- src/utils.py
+++ src/utils.py
@@ -4,23 +4,24 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from exception import CustomException
+from src.exception import CustomException
 
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+            pickle.dump(obj,file_obj)
 
     except Exception as e:
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,X_test,y_train,y_test,models,param):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
 
@@ -33,7 +34,7 @@ def evaluate_model(X_train,X_test,y_train,y_test,models,param):
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
@@ -43,3 +44,13 @@ def evaluate_model(X_train,X_test,y_train,y_test,models,param):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
